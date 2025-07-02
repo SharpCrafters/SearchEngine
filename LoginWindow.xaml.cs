5778f776306b3c0377e37f48681eee5058b17150
venezruk <venezruk@yandex.ru>
2025-07-02 23:14:21 +0300

    Rebuilding logic for form

--- LoginWindow.xaml.cs
+++ LoginWindow.xaml.cs
@@ -1,13 +1,8 @@
-﻿using System.Text;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using GalaSoft.MvvmLight.Command;
+using static MaterialDesignThemes.Wpf.Theme;
 
 namespace SearchEngine
 {
@@ -23,53 +18,97 @@ namespace SearchEngine
             InitializeComponent();
         }
 
-        private void UserNameTextBoxGotFocus(object sender, RoutedEventArgs e)
-        {
-            TextBox textBox = (TextBox)sender;
-            if (textBox.Text == "Имя пользователя")
-            {
-                textBox.Text = "";
-            }
-        }
-
-        private void UserNameTextBoxLostFocus(object sender, RoutedEventArgs e)
-        {
-            TextBox textBox = (TextBox)sender;
-            if (string.IsNullOrWhiteSpace(textBox.Text))
-            {
-                textBox.Text = "Имя пользователя";
-            }
-        }
-
-        private void PasswordTextBoxGotFocus(object sender, RoutedEventArgs e)
-        {
-            TextBox textBox = (TextBox)sender;
-            if (textBox.Text == "Пароль")
-            {
-                textBox.Text = "";
-            }
-        }
-
-        private void PasswordTextBoxLostFocus(object sender, RoutedEventArgs e)
-        {
-            TextBox textBox = (TextBox)sender;
-            if (string.IsNullOrWhiteSpace(textBox.Text))
-            {
-                textBox.Text = "Пароль";
-            }
-        }
-
         private void LoginButtonClick(object sender, RoutedEventArgs e)
         {
-            string UserName = UserNameTextBox.Text;
-            string UserPassword = PasswordTextBox.Text;
-
-            if ((UserName != null) && (UserPassword != null) && (UserName != "Имя пользователя") && (UserPassword != "Пароль"))
+            if ((UserNameTextBox.Text != null) && (UserPasswordBox.Password != null) && (UserNameTextBox.Text.ToString().Length > 0) && (UserPasswordBox.Password.ToString().Length > 0))
             {
-                bool IsUserExist = _DataBaseService.IsUserExist(UserName, UserPassword);
 
-                if (IsUserExist) this.Close();
-            } 
+                if (_DataBaseService.IsUserExist(UserNameTextBox.Text, UserPasswordBox.Password))
+                {
+                    Window SuccessfulMessage = null;
+
+                    SuccessfulMessage = new Window
+                    {
+                        Title = "Успешный вход",
+                        SizeToContent = SizeToContent.WidthAndHeight,
+                        ResizeMode = ResizeMode.NoResize,
+                        WindowStartupLocation = WindowStartupLocation.CenterScreen,
+                        Icon = Application.Current.MainWindow.Icon,
+                        Background = (Brush)new BrushConverter().ConvertFrom("#D9D9D9"),
+                        Content = new StackPanel
+                        {
+                            Margin = new Thickness(20),
+                            Children =
+                            {
+                                new TextBlock
+                                {
+                                    Text = $"Вы успешно вошли в систему в качестве пользователя \"{UserNameTextBox.Text}\"!",
+                                    HorizontalAlignment = HorizontalAlignment.Center,
+                                    FontSize = 16,
+                                    Margin = new Thickness(0, 0, 0, 20),
+                                    Foreground = (Brush)new BrushConverter().ConvertFrom("#373737")
+                                },
+                                new System.Windows.Controls.Button
+                                {
+                                    Background = (Brush)new BrushConverter().ConvertFrom("#373737"),
+                                    Content = "Отлично!",
+                                    HorizontalAlignment = HorizontalAlignment.Center,
+                                    BorderBrush = new SolidColorBrush(Color.FromRgb(55,55,55)),
+                                    Foreground = (Brush)new BrushConverter().ConvertFrom("#D9D9D9"),
+                                    Command = new RelayCommand(() => SuccessfulMessage.Close()),                            
+                                }
+                            }
+                        }
+                    };
+                    SuccessfulMessage.Closed += (s, e) => this.Close();
+
+                    SuccessfulMessage.ShowDialog();                    
+                }
+
+                else
+                {
+                    Window SuccessfulMessage = null;
+
+                    SuccessfulMessage = new Window
+                    {
+                        Title = "Неудачный вход",
+                        SizeToContent = SizeToContent.WidthAndHeight,
+                        ResizeMode = ResizeMode.NoResize,
+                        WindowStartupLocation = WindowStartupLocation.CenterScreen,
+                        Icon = Application.Current.MainWindow.Icon,
+                        Background = (Brush)new BrushConverter().ConvertFrom("#D9D9D9"),
+                        Content = new StackPanel
+                        {
+                            Margin = new Thickness(20),
+                            Children =
+                            {
+                                new TextBlock
+                                {
+                                    Text = $"Неверное имя пользователя или пароль :(",
+                                    HorizontalAlignment = HorizontalAlignment.Center,
+                                    FontSize = 16,
+                                    Margin = new Thickness(0, 0, 0, 20),
+                                    Foreground = (Brush)new BrushConverter().ConvertFrom("#373737")
+                                },
+                                new System.Windows.Controls.Button
+                                {
+                                    Background = (Brush)new BrushConverter().ConvertFrom("#373737"),
+                                    Content = "Очень жаль!",
+                                    HorizontalAlignment = HorizontalAlignment.Center,
+                                    BorderBrush = new SolidColorBrush(Color.FromRgb(55,55,55)),
+                                    Foreground = (Brush)new BrushConverter().ConvertFrom("#D9D9D9"),
+                                    Command = new RelayCommand(() => SuccessfulMessage.Close())
+                                }
+                            }
+                        }
+                    };
+                    SuccessfulMessage.Closed += (s, e) => this.UserPasswordBox.Password = "";
+
+                    SuccessfulMessage.ShowDialog();
+                }
+
+            }
         }
     }
 }
+

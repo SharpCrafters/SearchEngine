88a164d86b50b374b2e0f5e1cf925e1391ff8192
venezruk <venezruk@yandex.ru>
2025-07-12 01:51:28 +0300

    Optimized code

--- LoginWindow.xaml.cs
+++ LoginWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 using GalaSoft.MvvmLight.Command;
@@ -19,155 +22,83 @@ namespace SearchEngine
 
         private void LoginButtonClick(object sender, RoutedEventArgs e)
         {
-            if ((UserNameTextBox.Text != null) && (UserPasswordBox.Password != null) && (UserNameTextBox.Text.ToString().Length > 0) && (UserPasswordBox.Password.ToString().Length > 0))
+            if (string.IsNullOrEmpty(UserNameTextBox.Text) || string.IsNullOrEmpty(UserPasswordBox.Password))
             {
-
-                if (_DataBaseService.IsUserExist(UserNameTextBox.Text, UserPasswordBox.Password))
-                {
-                    if (UserNameTextBox.Text == "user")
-                    {
-
-                        Window SuccessfulMessage = null;
-
-                        UserMainWindow MainWindow = new UserMainWindow();
-
-                        SuccessfulMessage = new Window
-                        {
-                            Title = "Успешный вход",
-                            SizeToContent = SizeToContent.WidthAndHeight,
-                            ResizeMode = ResizeMode.NoResize,
-                            WindowStartupLocation = WindowStartupLocation.CenterScreen,
-                            Icon = Application.Current.MainWindow.Icon,
-                            Background = (Brush)new BrushConverter().ConvertFrom("#D9D9D9"),
-                            Content = new StackPanel
-                            {
-                                Margin = new Thickness(20),
-                                Children =
-                            {
-                                new TextBlock
-                                {
-                                    Text = $"Вы успешно вошли в систему в качестве пользователя \"{UserNameTextBox.Text}\"!",
-                                    HorizontalAlignment = HorizontalAlignment.Center,
-                                    FontSize = 16,
-                                    Margin = new Thickness(0, 0, 0, 20),
-                                    Foreground = (Brush)new BrushConverter().ConvertFrom("#373737")
-                                },
-                                new System.Windows.Controls.Button
-                                {
-                                    Background = (Brush)new BrushConverter().ConvertFrom("#373737"),
-                                    Content = "Отлично!",
-                                    HorizontalAlignment = HorizontalAlignment.Center,
-                                    BorderBrush = new SolidColorBrush(Color.FromRgb(55,55,55)),
-                                    Foreground = (Brush)new BrushConverter().ConvertFrom("#D9D9D9"),
-                                    Command = new RelayCommand(() => SuccessfulMessage.Close()),
-                                }
-                            }
-                            }
-                        };
-                        SuccessfulMessage.Closed += (s, e) => this.Close();
-
-                        SuccessfulMessage.Closed += (s, e) =>
-                        {
-                            MainWindow.Show();
-                        };
-
-                        SuccessfulMessage.ShowDialog();
-                    }
-
-                    else if (UserNameTextBox.Text == "admin")
-                    {
-                        Window SuccessfulMessage = null;
-
-                        AdministratorMainWindow MainWindow = new AdministratorMainWindow();
-
-                        SuccessfulMessage = new Window
-                        {
-                            Title = "Успешный вход",
-                            SizeToContent = SizeToContent.WidthAndHeight,
-                            ResizeMode = ResizeMode.NoResize,
-                            WindowStartupLocation = WindowStartupLocation.CenterScreen,
-                            Icon = Application.Current.MainWindow.Icon,
-                            Background = (Brush)new BrushConverter().ConvertFrom("#D9D9D9"),
-                            Content = new StackPanel
-                            {
-                                Margin = new Thickness(20),
-                                Children =
-                            {
-                                new TextBlock
-                                {
-                                    Text = $"Вы успешно вошли в систему в качестве пользователя \"{UserNameTextBox.Text}\"!",
-                                    HorizontalAlignment = HorizontalAlignment.Center,
-                                    FontSize = 16,
-                                    Margin = new Thickness(0, 0, 0, 20),
-                                    Foreground = (Brush)new BrushConverter().ConvertFrom("#373737")
-                                },
-                                new System.Windows.Controls.Button
-                                {
-                                    Background = (Brush)new BrushConverter().ConvertFrom("#373737"),
-                                    Content = "Отлично!",
-                                    HorizontalAlignment = HorizontalAlignment.Center,
-                                    BorderBrush = new SolidColorBrush(Color.FromRgb(55,55,55)),
-                                    Foreground = (Brush)new BrushConverter().ConvertFrom("#D9D9D9"),
-                                    Command = new RelayCommand(() => SuccessfulMessage.Close()),
-                                }
-                            }
-                            }
-                        };
-
-                        SuccessfulMessage.Closed += (s, e) => this.Close();
-
-                        SuccessfulMessage.Closed += (s, e) =>
-                        {
-                            MainWindow.Show();
-                        };
-
-                        SuccessfulMessage.ShowDialog();
-                    }
-                }
-
-                else
-                {
-                    Window SuccessfulMessage = null;
-
-                    SuccessfulMessage = new Window
-                    {
-                        Title = "Неудачный вход",
-                        SizeToContent = SizeToContent.WidthAndHeight,
-                        ResizeMode = ResizeMode.NoResize,
-                        WindowStartupLocation = WindowStartupLocation.CenterScreen,
-                        Icon = Application.Current.MainWindow.Icon,
-                        Background = (Brush)new BrushConverter().ConvertFrom("#D9D9D9"),
-                        Content = new StackPanel
-                        {
-                            Margin = new Thickness(20),
-                            Children =
-                            {
-                                new TextBlock
-                                {
-                                    Text = $"Неверное имя пользователя или пароль :(",
-                                    HorizontalAlignment = HorizontalAlignment.Center,
-                                    FontSize = 16,
-                                    Margin = new Thickness(0, 0, 0, 20),
-                                    Foreground = (Brush)new BrushConverter().ConvertFrom("#373737")
-                                },
-                                new System.Windows.Controls.Button
-                                {
-                                    Background = (Brush)new BrushConverter().ConvertFrom("#373737"),
-                                    Content = "Очень жаль!",
-                                    HorizontalAlignment = HorizontalAlignment.Center,
-                                    BorderBrush = new SolidColorBrush(Color.FromRgb(55,55,55)),
-                                    Foreground = (Brush)new BrushConverter().ConvertFrom("#D9D9D9"),
-                                    Command = new RelayCommand(() => SuccessfulMessage.Close())
-                                }
-                            }
-                        }
-                    };
-                    SuccessfulMessage.Closed += (s, e) => this.UserPasswordBox.Password = "";
-
-                    SuccessfulMessage.ShowDialog();
-                }
-
+                return;
             }
+
+            if (!_DataBaseService.IsUserExist(UserNameTextBox.Text, UserPasswordBox.Password))
+            {
+                ShowErrorMessage();
+                return;
+            }
+
+            switch (UserNameTextBox.Text)
+            {
+                case "user":
+                    ShowSuccessMessage("user", new UserMainWindow());
+                    break;
+                case "admin":
+                    ShowSuccessMessage("admin", new AdministratorMainWindow());
+                    break;
+            }
+        }
+
+        private void ShowErrorMessage()
+        {
+            var Message = CreateMessageWindow("Неверное имя пользователя или пароль :(", "Неудачный вход", true);
+            Message.Closed += (s, e) => UserPasswordBox.Password = "";
+            Message.ShowDialog();
+        }
+
+        private void ShowSuccessMessage(string RoleName, Window MainWindow)
+        {
+            var Message = CreateMessageWindow($"Вы успешно вошли в систему в качестве пользователя \"{RoleName}\"!", "Успешный вход");
+
+            Message.Closed += (s, e) => this.Close();
+            Message.Closed += (s, e) => MainWindow.Show();
+            Message.ShowDialog();
+        }
+
+        private Window CreateMessageWindow(string MessageText, string Title, bool IsError = false)
+        {
+            Window Window = null;
+
+            Window = new Window
+            {
+                Title = Title,
+                SizeToContent = SizeToContent.WidthAndHeight,
+                ResizeMode = ResizeMode.NoResize,
+                WindowStartupLocation = WindowStartupLocation.CenterScreen,
+                Icon = Application.Current.MainWindow.Icon,
+                Background = (Brush)new BrushConverter().ConvertFrom("#D9D9D9"),
+                Content = new StackPanel
+                {
+                    Margin = new Thickness(20),
+                    Children =
+            {
+                new TextBlock
+                {
+                    Text = MessageText,
+                    HorizontalAlignment = HorizontalAlignment.Center,
+                    FontSize = 16,
+                    Margin = new Thickness(0, 0, 0, 20),
+                    Foreground = (Brush)new BrushConverter().ConvertFrom("#373737")
+                },
+                new Button
+                {
+                    Background = (Brush)new BrushConverter().ConvertFrom("#373737"),
+                    Content = IsError ? "Очень жаль!" : "Отлично!",
+                    HorizontalAlignment = HorizontalAlignment.Center,
+                    BorderBrush = new SolidColorBrush(Color.FromRgb(55,55,55)),
+                    Foreground = (Brush)new BrushConverter().ConvertFrom("#D9D9D9"),
+                    Command = new RelayCommand(() => Window.Close()),
+                }
+            }
+                }
+            };
+
+            return Window;
         }
     }
 }
